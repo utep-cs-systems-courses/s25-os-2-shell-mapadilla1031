6439c2c353f8148e2c5ade2c08c07010869853da
mapadilla1031 <mapadilla10@miners.utep.edu>
2025-02-23 21:39:23 -0700

    changes to fork execute method

--- shell/myShell.py
+++ shell/myShell.py
@@ -4,49 +4,62 @@ import sys
 import re
 
 def shell_prompt():
-    return "$ "
+    return os.environ.get('PS1', '$ ')#shell prompt PS1 or $ 
 
 def fork_and_execute(command, args):
     pid = os.fork()
     
-    if pid < 0:  # if pid < 0, fork failed
-        os.write(2, ("The fork failed, returning %d\n" % pid).encode())
+    if pid < 0:  # The fork failed
+        os.write(2, f"Fork has failed with error code:  {pid}\n".encode())
         sys.exit(1)
         
-    elif pid == 0:  # if pid = 0, Child process
-        path = "/usr/local/bin:/usr/bin:/bin"
-        
-        for dir in re.split(":", path):
-            program = "%s/%s" % (dir, command)#run command in each directory path
+    elif pid == 0:  # The child process
+        for dir in os.environ['PATH'].split(':'):
+            program = "%s/%s" % (dir, command)  #string formatting
             try:
                 os.execve(program, [command] + args, os.environ)
             except FileNotFoundError:
-                pass
-                
-        os.write(2, f"{command}: The command was not found\n".encode())
+                pass  
+        os.write(2, f"{command}: command not found\n".encode())
         sys._exit(1)
         
-    else:  
-        return os.wait()#return to parent process
+    else:  #The parent process
+        childPidCode = os.wait()
+        if childPidCode[1] != 0:  
+            os.write(2, f"Program terminated with exit code: {childPidCode[1] >> 8}.\n".encode())#Shift right by 8 bits to get the actual exit code
+        return childPidCode[1]
 
 def main():
     while True:
         os.write(1, shell_prompt().encode())
         user_input = sys.stdin.readline()
+        
         if not user_input:
-            sys.exit(0)    
+            sys.exit(0)
             
         user_input = user_input.strip()
         if not user_input:
             continue
+            
         parts = user_input.split()
-        
         command = parts[0]
         args = parts[1:]
-
+        
+        if command == "cd":#cd command functionality
+            try:
+                if args:
+                    target_directory = args[0]#use the first one as the target directory
+                else:
+                    target_directory = os.environ['HOME']#otherwise use HOME ENV
+                os.chdir(target_directory)# Change to the target directory
+            except Exception as e:
+                os.write(2, f"cd: {str(e)}\n".encode())
+            continue
+    
         if command == "exit":
             sys.exit(0)
             
         fork_and_execute(command, args)
+
 if __name__ == "__main__":
     main()
